ff3109587099511d991b236708ba957da8e96b12
raphadvl <raphael.rdoval@gmail.com>
2021-08-08 16:37:09 -0300

    criação de telas

--- brechovirtual/src/App.js
+++ brechovirtual/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import {Navbar, Card, Dropdown} from '/components'
+import logo from '/images/logo2.png';
+import { casaco, casaco2, jeans, jeans2, tenis, tenis2 } from '/images';
 import './App.css';
 
-function App() {
+
+const products = {
+  "1" : {
+    "name": "Calça Jeans",
+    "images": [jeans, jeans2],
+    "description":"This is a pretty new black jean without any real damage",
+    "price": 56.99,
+    "category": "Calças"
+  },
+  "2" : {
+    "name": "Casaco",
+    "images": [casaco, casaco2],
+    "description":"This is a pretty new black jean without any real damage",
+    "price": 99.63,
+    "category": "Casaco"
+  },
+  "3" : {
+    "name": "Tênis",
+    "images": [tenis, tenis2],
+    "description":"This is a pretty new black jean without any real damage",
+    "price": 51.00,
+    "category": "Tênis"
+  }
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar><Dropdown/></Navbar>
+      <div class="jumbotron jumbotron-fluid bg-transparent" >
+        <div class="container">
+          <div class="row justify-content-center">
+            <img src={logo} width="300" height="150" class="d-inline-block align-top" alt=""/>
+          </div> 
+          <div class="row justify-content-center">
+            <p class="lead">Seu site para vendas e trocas de peças de roupas.</p>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row ">
+            {products.map((prod)=>{
+              return (
+                <Card product={prod}/>
+              )
+            })}
+        </div>
+      </div> 
+    </>
   );
 }
 
-export default App;
+
--- brechovirtual/src/components/card.js
+++ brechovirtual/src/components/card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import heart from '../../public/static/images/heart.png'
+import Carrousel from '../components/carrousel'
 
 
 export default function Card(props) {
@@ -9,13 +10,13 @@ export default function Card(props) {
         <div className="col-sm-4 d-flex justify-content-center">
             <div className="card shadow-lg p-3 mb-5 bg-white rounded bd-highlight justify-content-center" style="width: 20rem;">
                 <div className="text-center">
-                
+                    <Carrousel images={props.product.images}/>
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title"><a href="produto.html" >{props.name}</a> <span className="text-right">R${props.price}</span></h5>
-                    <p className="card-text">{props.description}</p>
+                    <h5 className="card-title"><a href="produto.html" >{props.product.name}</a> <span className="text-right">R${props.product.price}</span></h5>
+                    <p className="card-text">{props.product.description}</p>
                     <div className="text-left">
-                        <h6><b>{props.category}</b></h6>
+                        <h6><b>{props.product.category}</b></h6>
                     </div>
                     <div className="text-right">
                         <a  href="desejos.html" > 
