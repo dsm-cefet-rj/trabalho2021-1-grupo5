beff80f65b8007c5ba5bc1c2bd32ee19a379a84c
raphadvl <raphael.rdoval@gmail.com>
2021-08-08 15:23:27 -0300

    components modifications

--- brechovirtual/src/components/chat.js
+++ brechovirtual/src/components/chat.js
@@ -4,16 +4,20 @@ import ReactDOM from 'react-dom';
 export default function Chat(props) {
     return( 
         <>
-            <div className="container">
+           
                 <div className="col">
-                    <div className="d-flex shadow p-3 mb-5 bg-white rounded bd-highlight justify-content-start">
-                        <h4>
-		                    <p style='font-size:15px'> <b>{ props.userType}: { props.user.name } </b></p>
-		                    <p style='font-size:15px'> { props.message } </p>
-                        </h4>
-                    </div>
+                    {props.messages.map((message) => {
+                        return (
+                                <div className="d-flex shadow p-3 mb-5 bg-white rounded bd-highlight justify-content-start">
+                                    <h4>
+                                        <p style='font-size:15px'> <b>{ props.messages.userType}: { props.messages.userName } </b></p>
+                                        <p style='font-size:15px'> { props.messages.message } </p>
+                                    </h4>
+                                </div>
+                     )
+                    })}
                 </div>
-            </div>
+                       
         </>
     );
 }
