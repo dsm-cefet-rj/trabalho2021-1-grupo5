30b24f28b89b9f24c967e73b1a1409318fdf9142
glasscar46 <glasscar46@gmail.com>
2021-08-16 19:22:44 -0300

    react-hook-form added

--- brechovirtual/src/pages/Booking.js
+++ brechovirtual/src/pages/Booking.js
@@ -26,7 +26,7 @@ export default function Booking({ bookings, setBookings }){
 
         const newMessagesArray = product.messages.push(newMessage);
         setBookings({ ...product, messages: newMessagesArray });
-        setMessage("")
+        setMessage("/")
     }
     return(
         <>
