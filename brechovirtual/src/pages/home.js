0805bd0e8e9f9ad96a97e2d044111b095cbed32b
Luma Chen <lumadafonseca@gmail.com>
2021-08-17 22:56:31 -0300

    Adicionando mais um produto e filtrando os produtos por status de reserva

--- brechovirtual/src/pages/home.js
+++ brechovirtual/src/pages/home.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import Card from '../components/card'
-import Navbar from '../components/navbar'
-import logo from '../images/logo2.png';
-import jeans from '../images/jeans.jpeg';
-import jeans2 from '../images/jeans2.jpeg';
-import casaco from '../images/casaco.jpg';
-import casaco2 from '../images/casaco2.jpg';
-import tenis from '../images/tenis.jpeg';
-import tenis2 from '../images/tenis2.jpeg'
+import React from "react";
+import Card from "../components/card";
+import Navbar from "../components/navbar";
+import logo from "../images/logo2.png";
 
- 
- export default function Home(props) {
-   const renderProdcuct = (product) =>{
-     return(
-       <Card product={product} key={product.name}/>
-     )
-   }
-   return (
-     <>
-       <Navbar/>
-       <div class="jumbotron jumbotron-fluid bg-transparent" >
-         <div class="container">
-           <div class="row justify-content-center">
-             <img src={logo} width="300" height="150" class="d-inline-block align-top" alt=""/>
-           </div> 
-           <div class="row justify-content-center">
-             <p class="lead">Seu site para vendas e trocas de peças de roupas.</p>
-         </div>
-         </div>
-       </div>
-       <div class="container">
-         <div class="row ">{props.products.map(renderProdcuct)}</div>
-       </div> 
-     </>
-   );
- }
- 
+export default function Home(props) {
+  const renderProdcuct = (product) => {
+    return (
+      <React.Fragment>
+        {product.status !== "reservado" && (
+          <Card product={product} key={product.name} />
+        )}
+      </React.Fragment>
+    );
+  };
+  return (
+    <>
+      <Navbar />
+      <div class="jumbotron jumbotron-fluid bg-transparent">
+        <div class="container">
+          <div class="row justify-content-center">
+            <img
+              src={logo}
+              width="300"
+              height="150"
+              class="d-inline-block align-top"
+              alt=""
+            />
+          </div>
+          <div class="row justify-content-center">
+            <p class="lead">
+              Seu site para vendas e trocas de peças de roupas.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div className="row">{props.products.map(renderProdcuct)}</div>
+      </div>
+    </>
+  );
+}
