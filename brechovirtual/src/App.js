457afbfdd57ca30fde29faefe3e0a6b4fba16022
glasscar46 <glasscar46@gmail.com>
2021-08-21 12:48:02 -0300

    redux react-redux installed

--- brechovirtual/src/App.js
+++ brechovirtual/src/App.js
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Product from "./pages/Product";
 import ProductRegister from "./pages/ProductRegister";
