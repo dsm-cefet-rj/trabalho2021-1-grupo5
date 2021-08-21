7eb964040594cbdadc179d360b8b1eab09ba4c46
glasscar46 <glasscar46@gmail.com>
2021-08-21 19:33:23 -0300

    bug.....

--- brechovirtual/src/actions/index.js
+++ brechovirtual/src/actions/index.js
@@ -1,12 +1,3 @@
-export function addProduct(novoProduto){
-  return({
-      type:"ADDPRODUCT",
-      payload:novoProduto,
-  })  
-}
-export function deleteProduct(id){
-    return({
-        type:"DELETEPRODUCT",
-        payload:id,
-    })
+export const addProduct =()=>{
+    
 }
