f0ec51f098ada38b0761136c765ac091409dd224
raphadvl <raphael.rdoval@gmail.com>
2021-09-11 23:59:49 -0300

    remarking booking and seller

--- backend-brechovirtual/brechovirtual/app.js
+++ backend-brechovirtual/brechovirtual/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
-app.use('/bookings', bookingsRouter);
-app.use('/sellers', sellersRouter);
+//app.use('/bookings', bookingsRouter);
+//app.use('/sellers', sellersRouter);
 
 module.exports = app;
