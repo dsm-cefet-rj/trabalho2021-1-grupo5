5c3bdbeba1edfa82c6929ae56ab7f6a19c2c3dbe
raphadvl <raphael.rdoval@gmail.com>
2021-09-26 18:54:54 -0300

    authenticate login and logout

--- backend-brechovirtual/brechovirtual/routes/user.js
+++ backend-brechovirtual/brechovirtual/routes/user.js
@@ -4,8 +4,31 @@ const passport = require("passport-local-mongoose");
 var router = express.Router();
 var authenticate = require("../auth");
 
-router.route("/login").post((req, res, next) => {});
-router.get("/logout", (req, res, next) => {});
+router.use(bodyParser.json());
+
+router.post("/login", passport.authenticate('local'), (req, res, next) => {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+    res.redirect('/');
+});
+
+
+router.get("/logout", (req, res, next) => {
+    if (req.session) {
+        req.session.destroy();
+        res.clearCookie('session-id');
+        res.redirect('/');
+      }
+      else {
+        var err = new Error('You are not logged in!');
+        err.status = 403;
+        next(err);
+      }
+});
+
+
 
 router.get("/signup", (req, res, next) => {
     User.register(new User({username: req.body.username}), req.body.password, 
