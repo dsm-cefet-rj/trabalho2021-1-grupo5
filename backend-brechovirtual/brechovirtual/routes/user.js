8fe6fc3254fb32b0460947bfcad4ee91efa50f25
Luma Chen <lumadafonseca@gmail.com>
2021-09-26 16:48:58 -0300

    Feito o sing up de autenticação

--- backend-brechovirtual/brechovirtual/routes/user.js
+++ backend-brechovirtual/brechovirtual/routes/user.js
@@ -6,6 +6,28 @@ var authenticate = require("../auth");
 
 router.route("/login").post((req, res, next) => {});
 router.get("/logout", (req, res, next) => {});
-router.get("/signup", (req, res, next) => {});
+
+router.get("/signup", (req, res, next) => {
+    User.register(new User({username: req.body.username}), req.body.password, 
+    (err, user) => {
+        if(err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({err: err});
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({success: true, status: 'Registration Successful!'});
+                res.redirect('/'); //redireciona para a página principal
+            });
+        }
+    });
+});
 
 modules.exports = router;
+
+
+
+
+
